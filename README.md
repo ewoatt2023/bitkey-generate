f67df90af483a8395daa4b6aaf6d4c1ab3dd7bea
ewoatt2023 <125437291+ewoatt2023@users.noreply.github.com>
2023-02-28 20:00:55 -0800

    Update and rename README.md to main.go

--- README.md
+++
@@ -1,4 +0,0 @@
-# bitkey-generate
-#generate bitcoin private keys an addresses
-#Trying to fix this code because this code is not working https://github.com/saracen/bitcoin-all-key-generator 
-#We see if it works
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/btcsuite/btcutil"
+	"github.com/decred/dcrd/dcrec/secp256k1"
+)
+
+func main() {
+	// Print header
+	fmt.Printf("%64s %34s %34s\n", "Private", "Public", "Public Compressed")
+
+	// Initialise big numbers with small numbers
+	count, one := big.NewInt(0), big.NewInt(1)
+
+	// Create a slice to pad our count to 32 bytes
+	padded := make([]byte, 32)
+
+	// Loop forever because we're never going to hit the end anyway
+	for {
+		// Increment our counter
+		count.Add(count, one)
+
+		// Copy count value's bytes to padded slice
+		copy(padded[32-len(count.Bytes()):], count.Bytes())
+
+		// Get public key
+		_, public := secp256k1.PrivKeyFromBytes(padded)
+
+		// Get compressed and uncompressed addresses
+		caddr, _ := btcutil.NewAddressPubKey(public.SerializeCompressed(), &btcutil.AddressParams{
+			PubKeyHashAddrID: 0x00,
+			PrivateKeyID:     0x80,
+		})
+		uaddr, _ := btcutil.NewAddressPubKey(public.SerializeUncompressed(), &btcutil.AddressParams{
+			PubKeyHashAddrID: 0x00,
+			PrivateKeyID:     0x80,
+		})
+
+		// Print keys
+		fmt.Printf("%x %34s %34s\n", padded, uaddr.EncodeAddress(), caddr.EncodeAddress())
+	}
+}
+
